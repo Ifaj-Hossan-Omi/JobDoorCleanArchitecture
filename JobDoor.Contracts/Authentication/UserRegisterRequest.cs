4e6be88c7c76bbd4e73389edd05478058b2cccad
Md. Ifaj Hossan Omi <omiifajhossan@gmail.com>
2023-10-02 08:53:35 +0600

    Fresh

--- JobDoor.Contracts/Authentication/UserRegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using JobDoor.Domain.Enum;
-
-namespace JobDoor.Contracts.Authentication;
-
-public record UserRegisterRequest(
-    string Username, 
-    string Password, 
-    UserType Type
-);
--- JobDoor.Domain/Token/RefreshToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobDoor.Domain.Token;
-
-public class RefreshToken
-{
-    public int Id { get; set; }
-    public required string Token { get; set; }
-    public DateTime Created { get; set; } = DateTime.Now;
-    public DateTime Expires { get; set; }
-}
